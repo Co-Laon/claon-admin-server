4a9677ba2adf173c4f58a643fa39fb3e9faee5c5
Minsoo <als95.dev@gmail.com>
2023-03-12 01:44:14 +0900

    chore: init spring webflux project

---
+++ src/main/java/com/claon/ClaonAdminApplication.java
@@ -0,0 +1,13 @@
+package com.claon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaonAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaonAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claon/ClaonAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaonAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
