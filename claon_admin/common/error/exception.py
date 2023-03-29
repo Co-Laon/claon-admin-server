36e21be57b30d41f891f5ce31fb6c29bdb4185f4
minas <98951922+minas1222@users.noreply.github.com>
2023-03-29 14:29:30 +0900

    feat: add error code and exception handler (#17) (#27)

---
+++ claon_admin/common/error/exception.py
@@ -0,0 +1,65 @@
+from enum import Enum
+
+
+class ErrorCode(Enum):
+    # 400 Bad Request
+    ROW_ALREADY_EXIST = 40000
+
+    # 401 Unauthorized Error
+    NOT_ACCESSIBLE = 40100
+
+    # 404 Not Found Error
+    DATA_DOES_NOT_EXIST = 40400
+
+    # 409 Conflict
+    CONFLICT_STATE = 40900
+
+    # 422
+    UNPROCESSABLE_ENTITY = 42200
+
+    # 500 Internal Server Error
+    INTERNAL_SERVER_ERROR = 50000
+
+    # 503 Service Unavailable
+    SERVICE_UNAVAILABLE = 50300
+
+
+class BaseRuntimeException(Exception):
+    def __init__(self, code: ErrorCode, message: str):
+        self.code = code
+        self.message = message
+
+
+class BadRequestException(BaseRuntimeException):
+    def __init__(self, code: ErrorCode, message: str):
+        super().__init__(code, message)
+
+
+class UnauthorizedException(BaseRuntimeException):
+    def __init__(self, code: ErrorCode, message: str):
+        super().__init__(code, message)
+
+
+class NotFoundException(BaseRuntimeException):
+    def __init__(self, code: ErrorCode, message: str):
+        super().__init__(code, message)
+
+
+class ConflictException(BaseRuntimeException):
+    def __init__(self, code: ErrorCode, message: str):
+        super().__init__(code, message)
+
+
+class UnprocessableEntityException(BaseRuntimeException):
+    def __init__(self, code: ErrorCode, message: str):
+        super().__init__(code, message)
+
+
+class InternalServerException(BaseRuntimeException):
+    def __init__(self, code: ErrorCode, message: str):
+        super().__init__(code, message)
+
+
+class ServiceUnavailableException(BaseRuntimeException):
+    def __init__(self, code: ErrorCode, message: str):
+        super().__init__(code, message)
