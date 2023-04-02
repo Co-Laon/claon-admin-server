ee4596a8313b6adb740941af62221eff726915f8
Minsoo <37828653+als95@users.noreply.github.com>
2023-04-02 17:15:18 +0900

    feat: add example code for attribute converter (#36)

--- claon_admin/model/example.py
+++ claon_admin/model/example.py
@@ -1,10 +1,17 @@
 from pydantic import BaseModel
 
 
+class ExamplePropertyDto(BaseModel):
+    name: str
+    description: str
+
+
 class ExampleRequestDto(BaseModel):
     name: str
+    prop: ExamplePropertyDto
 
 
 class ExampleResponseDto(BaseModel):
     id: str
     name: str
+    prop: ExamplePropertyDto
