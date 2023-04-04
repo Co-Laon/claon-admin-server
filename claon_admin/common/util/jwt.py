bd8b06d529a9e6ef1baf8478d99f6886ec7a2ca1
Minsoo <als95.dev@gmail.com>
2023-04-04 19:58:12 +0900

    feat: add redis config and refac to save refresh token to redis

--- claon_admin/common/util/jwt.py
+++ claon_admin/common/util/jwt.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from claon_admin.common.error.exception import UnauthorizedException, InternalServerException
 from claon_admin.common.error.exception import ErrorCode
 from claon_admin.common.util.header import add_jwt_tokens
+from claon_admin.common.util.redis import save_refresh_token, find_user_id_by_refresh_token, delete_refresh_token
 from claon_admin.config.consts import ACCESS_TOKEN_EXPIRE_MINUTES, JWT_ALGORITHM, JWT_SECRET_KEY, \
     REFRESH_TOKEN_EXPIRE_MINUTES, JWT_REFRESH_SECRET_KEY, TIME_ZONE_KST
 from claon_admin.container import db
@@ -23,12 +24,19 @@ def create_access_token(user_id: str) -> str:
     return jwt.encode(to_encode, JWT_SECRET_KEY, JWT_ALGORITHM)
 
 
-def create_refresh_token() -> str:
+def create_refresh_token(user_id: str) -> str:
     to_encode = {
         "exp": datetime.now(TIME_ZONE_KST) + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
     }
 
-    return jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, JWT_ALGORITHM)
+    token = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, JWT_ALGORITHM)
+    save_refresh_token(token, user_id)
+    return token
+
+
+def reissue_refresh_token(refresh_token: str, user_id: str) -> str:
+    delete_refresh_token(refresh_token)
+    return create_refresh_token(user_id)
 
 
 def resolve_access_token(access_token: str) -> dict:
@@ -89,20 +97,28 @@ async def get_subject(
         access_payload = resolve_access_token(access_token)
         refresh_payload = resolve_refresh_token(refresh_token)
 
-        user = await UserRepository.find_by_id(session, access_payload.get("sub"))
-        if user is None:
-            raise UnauthorizedException(
-                ErrorCode.USER_DOES_NOT_EXIST,
-                "Not existing user account."
-            )
-
         if is_expired(access_payload):
             if is_expired(refresh_payload):
                 raise UnauthorizedException(
                     ErrorCode.INVALID_JWT,
                     "Both access token and refresh token are expired."
                 )
-            add_jwt_tokens(response, create_access_token(access_payload.get("sub")), create_refresh_token())
+
+            user_id = find_user_id_by_refresh_token(refresh_token)
+            if user_id is None:
+                raise UnauthorizedException(
+                    ErrorCode.INVALID_JWT,
+                    "Refresh token is not valid."
+                )
+
+            add_jwt_tokens(response, create_access_token(user_id), reissue_refresh_token(refresh_token, user_id))
+
+            user = await UserRepository.find_by_id(session, user_id)
+            if user is None:
+                raise UnauthorizedException(
+                    ErrorCode.USER_DOES_NOT_EXIST,
+                    "Not existing user account."
+                )
 
             return UserProfileDto(
                 profile_image=user.profile_image,
@@ -116,6 +132,13 @@ async def get_subject(
                 raise UnauthorizedException(
                     ErrorCode.INVALID_JWT,
                     "Refresh token is expired."
+                )
+
+            user = await UserRepository.find_by_id(session, access_payload.get("sub"))
+            if user is None:
+                raise UnauthorizedException(
+                    ErrorCode.USER_DOES_NOT_EXIST,
+                    "Not existing user account."
                 )
 
             return UserProfileDto(
