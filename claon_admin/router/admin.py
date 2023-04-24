bffff71970f60890b3984dff38161b153988952b
kyeorye lee <92leekr@naver.com>
2023-04-24 17:04:40 +0900

    fix: import error

--- claon_admin/router/admin.py
+++ claon_admin/router/admin.py
@@ -9,7 +9,7 @@ from claon_admin.config.auth import get_subject
 from claon_admin.container import db, Container
 from claon_admin.model.admin import LectorResponseDto, CenterResponseDto
 from claon_admin.model.auth import RequestUser
-from claon_admin.service.user import UserService
+from claon_admin.service.admin import AdminService
 
 router = APIRouter()
 
@@ -18,8 +18,8 @@ router = APIRouter()
 class AdminRouter:
     @inject
     def __init__(self,
-                 user_service: UserService = Depends(Provide[Container.user_service])):
-        self.user_service = user_service
+                 admin_service: AdminService = Depends(Provide[Container.admin_service])):
+        self.admin_service = admin_service
 
     @router.get('/lectors/approve', response_model=List[LectorResponseDto])
     async def find_approval_pending_lectors(self,
@@ -31,14 +31,14 @@ class AdminRouter:
                              lector_id: str,
                              session: AsyncSession = Depends(db.get_db),
                              subject: RequestUser = Depends(get_subject)):
-        return await self.user_service.approve_lector(lector_id, session, subject)
+        return await self.admin_service.approve_lector(lector_id, session, subject)
 
     @router.delete('/lectors/{lector_id}/reject')
     async def reject_lector(self,
                             lector_id: str,
                             session: AsyncSession = Depends(db.get_db),
                             subject: RequestUser = Depends(get_subject)):
-        return await self.user_service.reject_lector(lector_id, session, subject)
+        return await self.admin_service.reject_lector(lector_id, session, subject)
 
     @router.get('/center/approve', response_model=List[CenterResponseDto])
     async def find_approval_pending_centers(self,
@@ -50,11 +50,11 @@ class AdminRouter:
                              center_id: str,
                              session: AsyncSession = Depends(db.get_db),
                              subject: RequestUser = Depends(get_subject)):
-        return await self.user_service.approve_center(center_id, session, subject)
+        return await self.admin_service.approve_center(center_id, session, subject)
 
     @router.delete('/center/{center_id}/reject')
     async def reject_center(self,
                             center_id: str,
                             session: AsyncSession = Depends(db.get_db),
                             subject: RequestUser = Depends(get_subject)):
-        return await self.user_service.reject_center(center_id, session, subject)
+        return await self.admin_service.reject_center(center_id, session, subject)
