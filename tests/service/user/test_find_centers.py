7b6fe82a1e35fe4bde7ca5c838e15e92943900f1
Minsoo <als95.dev@gmail.com>
2023-08-03 22:00:53 +0900

    refac: change error message for find centers

--- tests/service/user/test_find_centers.py
+++ tests/service/user/test_find_centers.py
@@ -1,7 +1,7 @@
 import pytest
 
 from claon_admin.common.enum import Role
-from claon_admin.common.error.exception import UnauthorizedException, ErrorCode
+from claon_admin.common.error.exception import ErrorCode, NotFoundException
 from claon_admin.model.auth import RequestUser
 from claon_admin.model.user import CenterNameResponseDto
 
@@ -24,13 +24,32 @@ class TestFindCenters(object):
             other_center_fixture: Center
     ):
         # given
-        request_user = RequestUser(id=user_fixture.id, sns="test@claon.com", role=Role.LECTOR)
+        request_user = RequestUser(id=user_fixture.id, sns="test@claon.com", role=Role.CENTER_ADMIN)
         result_center = [center_fixture, other_center_fixture]
         mock_repo["center"].find_by_user_id.side_effect = [result_center]
         response = [CenterNameResponseDto.from_entity(entity=e) for e in result_center]
 
         # when
-        result = await user_service.find_centers(subject=request_user)
+        result = await user_service.find_centers(request_user)
 
         # then
         assert result == response
+
+    @pytest.mark.asyncio
+    @pytest.mark.it("Fail case: not found centers")
+    async def test_find_centers_with_not_centers(
+            self,
+            user_service: UserService,
+            mock_repo: dict,
+            user_fixture: User
+    ):
+        # given
+        request_user = RequestUser(id=user_fixture.id, sns="test@claon.com", role=Role.CENTER_ADMIN)
+        mock_repo["center"].find_by_user_id.side_effect = [[]]
+
+        with pytest.raises(NotFoundException) as exception:
+            # when
+            await user_service.find_centers(request_user)
+
+        # then
+        assert exception.value.code == ErrorCode.DATA_DOES_NOT_EXIST
